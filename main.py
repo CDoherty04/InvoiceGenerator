907701fe3b66290c2fbfbe83b019453c2321865a
Charlie Doherty <charlieedoherty@gmail.com>
2024-02-26 10:32:27 -0600

    Create PDF with all content

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 import pandas as pd
 import glob
+import fpdf
+import pathlib
 
+# Get files
 filepaths = glob.glob("invoices/*.xlsx")
 
+# Create a PDF
+document = fpdf.FPDF("P", "mm", "A4")
+
 for filepath in filepaths:
+    # Extract data from excel sheets
     df = pd.read_excel(filepath, sheet_name="Sheet 1")
 
-    print(df)
+    # Get the file name for naming and indexing
+    filename = pathlib.Path(filepath).stem
+    filenum = filename.split("-")[0]
+
+    # Create the output PDF
+    document.add_page()
+    document.set_font("Times", "B", 24)
+    document.cell(w=5, h=1, txt=f"Invoice {filenum}")
+
+document.output("PDFs/invoice.pdf")
